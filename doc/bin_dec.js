e682535c9764b037f2a5c308e2f7b6d74d47c331
Saroj Kumar Sahoo <41164464+sarojkumar007@users.noreply.github.com>
2020-11-16 12:00:17 +0530

    refresh push

---
+++ doc/bin_dec.js
@@ -0,0 +1,19 @@
+function decToBin(n){
+    let arr = [];
+    while(n>0){
+        arr.push(n%2);
+        n = Math.floor(n/2);
+    }
+    return parseInt(arr.reverse().join(''));
+}
+s
+
+function binToDec(n){
+    let arr = [];
+    let sum = 0;
+    arr = n.toString().split('').reverse();
+    for(let i=arr.length-1; i>=0; i--){
+        sum += arr[i]*Math.pow(2,i);
+    }
+    return sum;
+}
