927bd841c3710f7ed7ec821550451535f1ea967c
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2023-01-08 17:40:14 +0530

    added new project

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,14 @@
 const path = require(`path`);
-const readingTime = require("reading-time");
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const readingTime = require('reading-time');
+const { createFilePath } = require(`gatsby-source-filesystem`);
 const slugify = require('./src/helper/slugify.js');
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   // Define a template for blog post
-  const blogTemplate = path.resolve(`./src/templates/blogTemplate.js`)
-  const tagTemplate = path.resolve(`./src/templates/tagTemplate.js`)
+  const blogTemplate = path.resolve(`./src/templates/blogTemplate.js`);
+  const tagTemplate = path.resolve(`./src/templates/tagTemplate.js`);
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
@@ -23,24 +23,24 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             fields {
               slug
             }
-            frontmatter{
+            frontmatter {
               tags
             }
           }
         }
       }
     `
-  )
+  );
 
   if (result.errors) {
     reporter.panicOnBuild(
       `There was an error loading your blog posts`,
       result.errors
-    )
-    return
+    );
+    return;
   }
 
-  const posts = result.data.allMarkdownRemark.nodes
+  const posts = result.data.allMarkdownRemark.nodes;
 
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
@@ -48,25 +48,26 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   if (posts.length > 0) {
     posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+      const previousPostId = index === 0 ? null : posts[index - 1].id;
+      const nextPostId =
+        index === posts.length - 1 ? null : posts[index + 1].id;
 
       const slugWithoutSlash = post.fields.slug.replace('/', '');
 
       const tagSet = new Set();
       post.frontmatter.tags.forEach(t => tagSet.add(t));
 
-      const tagList = Array.from(tagSet)
+      const tagList = Array.from(tagSet);
 
       tagList.forEach(t => {
         createPage({
           path: `/blog/tags/${slugify(t)}`,
           component: tagTemplate,
           context: {
-            tag: t
-          }
-        })
-      })
+            tag: t,
+          },
+        });
+      });
 
       createPage({
         path: `/blog/${slugWithoutSlash}`,
@@ -76,17 +77,16 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           previousPostId,
           nextPostId,
         },
-      })
-    })
+      });
+    });
   }
-
-}
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
 
     createNodeField({
       node,
@@ -99,10 +99,10 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value: readingTime(node.rawMarkdownBody),
     });
   }
-}
+};
 
 exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
+  const { createTypes } = actions;
 
   // Explicitly define the siteMetadata {} object
   // This way those will always be defined even if removed from gatsby-config.js
@@ -140,5 +140,5 @@ exports.createSchemaCustomization = ({ actions }) => {
     type Fields {
       slug: String
     }
-  `)
-}
+  `);
+};
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react'
-import { X } from 'react-feather'
+import React, { useEffect } from 'react';
+import { X } from 'react-feather';
 
 const Modal = ({ id, children, isOpen, handleClose }) => {
+  useEffect(() => {
+    if (isOpen) {
+      window.document.body.style.overflow = 'hidden';
+    } else {
+      window.document.body.style.overflow = null;
+    }
+    return () => {
+      window.document.body.style.overflow = null;
+    };
+  }, [isOpen]);
 
-	useEffect(() => {
-		if (isOpen) {
-			window.document.body.style.overflow = 'hidden';
-		} else {
-			window.document.body.style.overflow = null;
-		}
-		return () => {
-			window.document.body.style.overflow = null;
-		}
-	}, [isOpen])
+  return (
+    <>
+      <div className={`modal ${isOpen ? 'active' : ''}`} id={id}>
+        <div className="modal__overlay" onClick={handleClose}></div>
+        <div className="modal__content">
+          <button className="modal__close" onClick={handleClose}>
+            <X />
+          </button>
+          <div className="modal__body">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-	return (
-		<>
-			<div className={`modal ${isOpen ? 'active' : ''}`} id={id}>
-				<div className="modal__overlay"></div>
-				<div className="modal__content">
-					<button className="modal__close" onClick={handleClose}>
-						<X />
-					</button>
-					<div className="modal__body">{children}</div>
-				</div>
-			</div>
-		</>
-	)
-}
-
-export default Modal
+export default Modal;
