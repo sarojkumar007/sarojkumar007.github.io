f4969e54cbc26095722b8dc2169e8881af6a431c
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2023-06-26 01:03:47 +0530

    Integrate Ads, stage2

--- src/components/short/GoogleAds.tsx
+++ src/components/short/GoogleAds.tsx
@@ -6,7 +6,23 @@ const GoogleAds: FC<any> = (props: any) => {
 
   return (
     <>
-      <div className={`g-ads w-full ${classProps}`} {...otherProps}></div>
+      <div className={`g-ads w-full ${classProps}`} {...otherProps}>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9840063743847527"
+          crossOrigin="anonymous"
+        ></script>
+        {/* SidebarAds */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-9840063743847527"
+          data-ad-slot="3126839442"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      </div>
     </>
   )
 }
