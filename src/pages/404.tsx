90edaed14c119f5f1d0942bc1d8b1da566b4eea1
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2023-07-09 21:41:32 +0530

    Update, 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,11 +3,31 @@ import * as React from "react"
 import Layout from "@components/Layout"
 import Seo from "@components/Seo"
 
+import Svg404 from '@components/svgs/Icon404';
+import { Link } from "gatsby";
+
 const NotFoundPage = () => {
   return (
     <Layout>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <header className="min-h-[90vh] grid place-items-center p-8">
+        <div className="grid grid-cols-1">
+          <figure className="w-full">
+            <Svg404 className="w-full h-full" />
+          </figure>
+          <div className="mt-4 text-center">
+            <h3 className="text-3xl font-sailec font-medium">
+              The page doesn't exits !
+            </h3>
+            <p className="text-base text-gray-500">
+              Try going Back Or read some{' '}
+              <Link to={`/blog`} className="global_link">
+                Blog
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </header>
     </Layout>
   )
 }
