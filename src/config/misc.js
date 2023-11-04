3390074eb7a4a262df743730007d4e34d0f13c23
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2023-11-04 13:50:10 +0530

    updated quotes

--- src/config/misc.js
+++ src/config/misc.js
@@ -12,4 +12,17 @@ export const quotes = [
     author: "Lao Tzu",
   },
   { quote: "Do, or do not. There is no try.", author: "Yoda" },
+  {
+    quote: "The cave you fear to enter holds the treasure you seek.",
+    author: "Joseph Campbell",
+  },
+  {
+    quote:
+      "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  {
+    quote: "If you want to fly, give up everything that weighs you down.",
+    author: "Buddha",
+  },
 ]
